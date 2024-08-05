9e3a269cb34b7df74149ad537a6b6765a89a624b
DanielGarciaPluma <230187@utxicotepec.edu.mx>
2024-08-05 02:42:41 -0600

    Agregacion de los Archivos de Scripts
    se agregaron todos los archivos de Scripts

---
+++ Databases/Scripts/InsertarAdministradores.sql
@@ -0,0 +1,7 @@
+-- ============================ 
+-- SCRIPT InsertarAdministradores 
+-- ============================ 
+INSERT INTO Administrador (Nombre, Apellido_Paterno, Apellido_Materno, Usuario, Contrase¤a, Email) VALUES 
+('Ana', 'Garc¡a', 'Mart¡nez', 'admin1', 'adminpass1', 'ana.garcia@example.com'), 
+('Luis', 'Rodr¡guez', 'Fern ndez', 'admin2', 'adminpass2', 'luis.rodriguez@example.com'), 
+('Carlos', 'Mart¡nez', 'Hern ndez', 'admin3', 'adminpass3', 'carlos.martinez@example.com'); 
---
+++ Databases/Scripts/InsertarUsuarioVisitantes.sql
@@ -0,0 +1,14 @@
+-- ============================ 
+-- SCRIPT InsertarUsuarioVisitantes 
+-- ============================ 
+INSERT INTO Usuario_Visitante (Nombre) VALUES 
+('David'), 
+('Olga'), 
+('Miguel'), 
+('Sandra'), 
+('Javier'), 
+('Natalia'), 
+('Alejandro'), 
+('Carla'), 
+('Ricardo'), 
+('Isabel'); 
