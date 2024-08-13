f7d0b498ae80ce4da9ecb50e032f8c53da136622
ppyo1234 <230629@utxicotepec.edu.mx>
2024-08-12 22:25:30 -0600

    Actualización de BackEnd
    Se actulizó la conección de la base de datos al php y se agregaron ejemplos

---
+++ BackEnd/PHP/registrar.php
@@ -0,0 +1,25 @@
+<?php 
+
+include("con_db.php");
+//aqui ira toda la captura de datos para el backEnd
+if (isset($_POST['valores'])) {
+    if (strlen($_POST['valores']) >= 1 && strlen($_POST['valores']) >= 1) {
+
+	    $resultado = mysqli_query($conex,$consulta);
+	    if ($resultado) {
+	    	?> 
+	    	<h3 class="ok">¡Te has inscrito correctamente!</h3>
+           <?php
+	    } else {
+	    	?> 
+	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
+           <?php
+	    }
+    }   else {
+	    	?> 
+	    	<h3 class="bad">¡Por favor complete los campos!</h3>
+           <?php
+    }
+}
+
+?>
