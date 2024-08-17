1fcf31ede5acbd62b4ae6ccc0a19e608b4205672
DanielGarciaPluma <230187@utxicotepec.edu.mx>
2024-08-16 22:29:06 -0600

    Actualizacion de los Archivos de Scripts
    se actualizo los archivos de la carpeta Scripts por modificaciones

--- Databases/Scripts/InsertarAdministradores.sql
+++
@@ -1,7 +0,0 @@
--- ============================ 
--- SCRIPT InsertarAdministradores 
--- ============================ 
-INSERT INTO Administrador (Nombre, Apellido_Paterno, Apellido_Materno, Usuario, Contrase¤a, Email) VALUES 
-('Ana', 'Garc¡a', 'Mart¡nez', 'admin1', 'adminpass1', 'ana.garcia@example.com'), 
-('Luis', 'Rodr¡guez', 'Fern ndez', 'admin2', 'adminpass2', 'luis.rodriguez@example.com'), 
-('Carlos', 'Mart¡nez', 'Hern ndez', 'admin3', 'adminpass3', 'carlos.martinez@example.com'); 
---
+++ Databases/Scripts/InsertarUsuarios.sql
@@ -0,0 +1,14 @@
+-- ============================ 
+-- SCRIPT InsertarUsuarioAdoptantes 
+-- ============================ 
+INSERT INTO Usuario (Nombre, Apellido_Paterno, Apellido_Materno, Direcci¢n, Tel‚fono, Email, Rol Fecha_Registro) VALUES 
+('Daniel', 'Garcia', 'Pluma', 'Calle Bocanegra 8', '5551967835', 'daniel.garcia@example.com', 'Administrado', '2024-01-01'), 
+('Dulce', 'Salvador', 'Antonio', 'Avenida Siempre Viva 742', '555-0002', 'dulce.salvador@example.com', 'Administrado', '2024-01-10'), 
+('Arturo', 'Garcia', 'Gonzalez', 'Calle Real 456', '555-0003', 'arturo.garcia@example.com', 'Blogger', '2024-02-01'), 
+('Sayurid', 'Bautista', 'Cruz', 'Avenida Libertad 789', '555-0004', 'sayurid.bautista@example.com', 'Blogger', '2024-02-15'), 
+('Luis', 'Rodr¡guez', 'Torres', 'Calle de la Paz 123', '555-0005', 'luis.rodriguez@example.com', 'Usuario', '2024-03-01'), 
+('Claudia', 'Mora', 'Cordero', 'Avenida Uni¢n 456', '555-0006', 'claudia.mora@example.com', 'Usuario', '2024-03-10'), 
+('Carlos', 'Hern ndez', 'Ram¡rez', 'Calle de los Sue¤os 789', '555-0007', 'carlos.hernandez@example.com', 'Usuario', '2024-04-01'), 
+('Sof¡a', 'S nchez', 'R¡os', 'Avenida Esperanza 123', '555-0008', 'sofia.sanchez@example.com', 'Usuario', '2024-04-15'), 
+('Jorge', 'Gonz lez', 'Ramos', 'Calle del Mar 456', '555-0009', 'jorge.gonzalez@example.com', 'Usuario', '2024-05-01'), 
+('Laura', 'Paredes', 'Salinas', 'Avenida de los Pinos 789', '555-0010', 'laura.paredes@example.com', 'Usuario', '2024-05-15'); 
