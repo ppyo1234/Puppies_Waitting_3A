ff4f494192e62e665cd78f236556a5790d4b8f53
ppyo1234 <230629@utxicotepec.edu.mx>
2024-08-23 11:32:24 -0600

    Actualización de información

--- BackEnd/DB_Connection/con_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conex = mysqli_connect("localhost","root","1234","puppies_waiting"); 
-
-?>
--- Databases/Triggers/ActualizarFechaPublicacionAntesInsert_Trigger.sql
+++
@@ -1,3 +0,0 @@
-CREATE DEFINER=`root`@`localhost` TRIGGER `ActualizarFechaPublicacionAntesInsert` BEFORE INSERT ON `contenido` FOR EACH ROW BEGIN
-    SET NEW.Fecha_Publicación = IFNULL(NEW.Fecha_Publicación, CURDATE());
-END
